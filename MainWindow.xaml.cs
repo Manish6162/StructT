ce4cf9263bcd156736342dad227dd0f49374596f
manee <manee@RANVEER>
2022-10-20 20:43:32 +0530

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace StructT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private T[] CreateArray<T>(T firstElement, T secondElement) where T : ICloneable, new()
+        {
+            return new T[] { firstElement, secondElement };          
+        }
+    }
+}
